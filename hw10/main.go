0e7b04fa015ce40cc6640f61112c13ce5a05123f
Oksana Hladunska <oksana.hladunska@gmail.com>
2024-08-05 17:06:16 +0300

    Update main.go

--- hw10/main.go
+++ hw10/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"log"
-	"main/hw10/router"
+	"github.com/gorilla/mux"
+        "main/hw10/handler"
+	"main/hw10/models"
 	"net/http"
 )
 
 func main() {
-	r := router.InitializeRouter()
+	taskManager := models.NewTaskManager()
+    taskHandlers := handler.NewTaskHandlers(taskManager)
 
+    r := mux.NewRouter()
+    r.HandleFunc("/tasks", taskHandlers.CreateTask).Methods("POST")
+    r.HandleFunc("/tasks/{id}", taskHandlers.GetTask).Methods("GET")
+    r.HandleFunc("/tasks/{id}", taskHandlers.UpdateTask).Methods("PUT")
+    r.HandleFunc("/tasks/{id}", taskHandlers.DeleteTask).Methods("DELETE")
+    r.HandleFunc("/tasks", taskHandlers.GetAllTasks).Methods("GET")
+	
 	log.Println("Server started on: http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
